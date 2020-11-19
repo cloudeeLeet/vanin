d4009e0bfbf7590c8f6d640c69f746fd63aa6c5e
zaqcLeet <68500215+zaqcLeet@users.noreply.github.com>
2020-11-19 23:07:58 +0300

    dark-mode v1.3

--- script.js
+++ script.js
@@ -7,6 +7,10 @@ function addListenerToAddTask(){
         $('p').toggleClass('dark-mode');
         $('h1').toggleClass('dark-mode');
         $('a').toggleClass('dark-mode-a');
+        $('#btn').toggleClass('dark-mode-btn');
+        $('#btn-').toggleClass('dark-mode-btn-pdv');
+        $('#btn--').toggleClass('dark-mode-btn');
+        $('#btn---').toggleClass('dark-mode-btn');
         $('div#dark').toggleClass('dark-mode-bg');
         $('img').toggleClass('dark-mode-img');
     });
