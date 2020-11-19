d6299bb610e1ff42a0a7b0269f9d94c58ed928aa
zaqcLeet <68500215+zaqcLeet@users.noreply.github.com>
2020-11-19 22:16:55 +0300

    dark-mode v1.0

---
+++ script.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+    addListenerToAddTask();
+});
+
+function addListenerToAddTask(){ 
+    $('.slider').click(function(){
+        $('p').toggleClass('dark-mode');
+        $('h1').toggleClass('dark-mode');
+        $('a').toggleClass('dark-mode-a');
+        $('div#dark').toggleClass('dark-mode-bg');
+    });
+ }
